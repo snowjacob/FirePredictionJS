b2bf43f2cbb2ffcefca2a621954c58380bb2868d
Jacob S <43625307+snowjacob@users.noreply.github.com>
2023-06-15 00:33:43 -0700

    Create server.js

---
+++ local-server/server.js
@@ -0,0 +1,12 @@
+let express = require("express");
+let app = express();
+
+app.use(function(req, res, next){
+    console.log(`${new Date()} - ${req.method} request for ${req.url}`);
+    next();
+});
+
+app.use(express.static("../static"));
+app.listen(32, function() {
+    console.log("Serving static on 32");
+});
