f7809d6b257340bbb80723ddb058380e76aac0a6
Jacob S <43625307+snowjacob@users.noreply.github.com>
2023-07-30 22:36:58 -0700

    Update server.js

--- local-server/server.js
+++ local-server/server.js
@@ -1,12 +1,14 @@
 let express = require("express");
 let app = express();
+const port = process.env.PORT || 3000;
 
-app.use(function(req, res, next){
-    console.log(`${new Date()} - ${req.method} request for ${req.url}`);
-    next();
+app.use(function(req, res, next) {
+  console.log(`${new Date()} - ${req.method} request for ${req.url}`);
+  next();
 });
 
 app.use(express.static("../static"));
-app.listen(32, function() {
-    console.log("Serving static on 32");
+
+app.listen(port, function() {
+  console.log(`Serving static on port ${port}`);
 });
